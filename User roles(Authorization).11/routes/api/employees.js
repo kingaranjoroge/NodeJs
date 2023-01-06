cd5e1e33b629b3e8ba547d36684383dc403160ca
kingaranjoroge <briankingara4@gmail.com>
2023-01-06 18:56:31 +0300

    first commit

---
+++ User roles(Authorization).11/routes/api/employees.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const employeesController = require('../../controllers/employeesController')
+const ROLES_LIST = require('../../config/roles_list')
+const verifyRoles = require('../../middleware/verifyRoles')
+
+const router = express.Router()
+
+router.route('/')
+    .get(employeesController.getAllEmployees)
+    .post(verifyRoles(ROLES_LIST.Admin, ROLES_LIST.Editor), employeesController.createNewEmployees)
+    .put(verifyRoles(ROLES_LIST.Admin, ROLES_LIST.Editor), employeesController.updateEmployee)
+    .delete(verifyRoles(ROLES_LIST.Admin), employeesController.deleteEmployee)
+
+router.route('/:id')
+    .get(employeesController.getEmployee)
+
+
+module.exports = router
