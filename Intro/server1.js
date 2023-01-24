895de809722b0f109545f6391e90e0a00c8bf09c
kingaranjoroge <briankingara4@gmail.com>
2023-01-24 13:50:31 +0300

    made changes to some files

--- Intro/server1.js
+++ Intro/server1.js
@@ -1,20 +1,20 @@
 
 // INTRO
 
-/* console.log('Hello World')
-console.log(global) */
+console.log('Hello World')
+//console.log(global) 
 const os = require('os')
 const path = require('path')
-const math = require('./math')
-const { add, subtract, multiply, divide } = require('./math') 
+const math2 = require('./math2')
+const { add, subtract, multiply, divide } = require('./math2') 
 
-console.log(math.add(2, 3))
+console.log(math2.add(2, 3))
 console.log(add(2, 3))
 console.log(subtract(2, 3))
 console.log(multiply(2, 3))
 console.log(divide(2, 3))
 
-/* console.log(os.type())
+console.log(os.type())
 console.log(os.version())
 console.log(os.homedir())
 
@@ -25,5 +25,5 @@ console.log(path.dirname(__filename))
 console.log(path.basename(__filename))
 console.log(path.extname(__filename))
 
-console.log(path.parse(__filename)) */
+console.log(path.parse(__filename)) 
 
